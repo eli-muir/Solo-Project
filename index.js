53d2e8f4936ff417c6b1f95146141883f9fdded5
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-07-26 17:54:47 -0400

    added rough dropdown table and found some veggie data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //This will act as entry point for our webpack.
 import React from "react";
-import reactDom from "react-dom";
+import {render} from 'react-dom'
 import App from "./src/App";
 
 import styles from './style.css';
 
-reactDom.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ plant_list.js
@@ -0,0 +1,46 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+"radish": "Plant Radish between March 25 and May 1 in the spring, or in the fall between August 1 and 20 . Plant them a half inch deep, 1 inch apart, with 1 and one half feet between rows. It will take 25 to 30 days to mature.",
+"mustard": "Plant Mustard between March 25 and May 1 in the spring, or in the fall between August 1 and August 30 . Plant them a half inch deep, 1 inch apart, with 2 feet between rows. It will take 40 to 50 days to mature.",
+"beet": "Plant Beets between April 1 and 15 in the spring, or in the fall between August 1 and September 25 . Plant them one inch deep, 2 inches apart, with 2 to 2 and one half feet between rows. It will take 55 to 65 days to mature.",
+"spinach": "Plant Spinach between April 1 and 20 in the spring, or in the fall between August 10 and 20 . Plant them three quarters of an inch deep, 1 to 2 inches apart, with 1 and one half to 2 feet between rows. It will take 40 to 45 days to mature.",
+"turnip": "Plant Turnip between March 25 and May 1 in the spring, or in the fall between August 5 and September 20 . Plant them a half inch deep, 1 to 2 inches apart, with 1 to 2 feet between rows. It will take 45 to 65 days to mature.",
+"beans": "Plant Bean, bush between April 25 and May 30 in the spring, or in the fall between July 25 and August 5 . Plant them one to one and one half inches deep, 2 to 4 inches apart, with 3 feet between rows. It will take 50 to 60 days to mature.",
+"lettuce": "Plant Lettuce between April 1 and May 15 in the spring, or in the fall between September 1 and 15 . Plant them a half inch deep, 10 to 12 inches apart, with 2 to 2 and one half feet between rows. It will take 60 to 85 days to mature.",
+"kohlrabi ": "Plant Kohlrabi between April 1 and 15 in the spring, or in the fall between September 20 and 25 . Plant them a half inch deep, 10 inches apart, with 3 feet between rows. It will take 50 to 70 days to mature.",
+"carrot": "Plant Carrots between March 25 and April 10 in the spring, or in the fall between September 20 and 30 . Plant them a half inch deep, 2 to 3 inches apart, with 2 feet between rows. It will take 70 to 80 days to mature.",
+"onion": "Plant Onions between March 25 and April 15 in the spring, or in the fall between September 1 and December 31 . Plant them three quarters of an inch deep, 3 to 4 inches apart, with 1 to 2 feet between rows. It will take 100 to 120 days to mature.",
+"cabbage": "Plant Cabbage between April 1 and 20 in the spring, or in the fall between September 20 and 30 . Plant them one inch deep, 12 inches apart, with 2 and one half feet between rows. It will take 65 to 80 days to mature.",
+"cauliflower": "Plant Cauliflower between April 1 and 20 in the spring, or in the fall between September 20 and 30 . Plant them one inch deep, 12 to 18 inches apart, with 3 feet between rows. It will take 55 to 60 days to mature.",
+"broccoli": "Plant Broccoli between March 25 and April 5 in the spring, or in the fall between September 25 and 30 . Plant them one inch deep, 14 to 18 inches apart, with 2 and one half feet between rows. It will take 60 to 80 days to mature.",
+"asparagus": "Plant Asparagus between April 5 and 25 in the spring. Plant them 6 inches deep, one and one half to 2 feet apart, with 3 to 5 feet between rows. Youll have to wait till the second season to get a mature plant.",
+"pole beans": "Plant Bean, pole between May 10 and 20 in the spring. Plant them one to one and one half inches deep, 6 to 12 inches apart, with 3 feet between rows. It will take 65 to 75 days to mature.",
+"lima beans": "Plant Bean, lima between May 10 and 25 in the spring. Plant them one to one and one half inches deep, 3 to 4 inches apart, with 2 to 2 and one half feet between rows. It will take 65 to 75 days to mature.",
+"cantaloupe": "Plant Cantaloupe between May 10 and 20 in the spring. Plant them one and one half inches deep, 3 and a half to 4 feet apart, with 4 to 6 feet between rows. It will take 80 to 90 days to mature.",
+"collard": "Plant Collard between March 20 and April 10 in the spring. Plant them a half inch deep, 8 to 16 inches apart, with 2 and one half feet between rows. It will take 55 to 70 days to mature.",
+"corn": "Plant Corn between May 1 and July 20 in the spring. Plant them two inches deep, 12 to 18 inches apart, with 3 to 3 and one half feet between rows. It will take 80 to 100 days to mature.",
+"cucumber": "Plant Cucumbers between May 10 and 30 in the spring. Plant them one and one half inches deep, 3 to 4 feet apart, with 3 and one half to 5 feet between rows. It will take 60 to 65 days to mature.",
+"eggplant": "Plant Eggplant between May 15 and 25 in the spring. Plant them one inch deep, 2 and one half to 3 feet apart, with 3 feet between rows. It will take 75 to 90 days to mature.",
+"kale": "Plant Kale between March 25 and April 5 in the spring. Plant them a half inch deep, 10 inches apart, with 3 feet between rows. It will take 50 to 70 days to mature.",
+"okra": "Plant Okra between May 10 and 25 in the spring. Plant them one inch deep, 6 inches apart, with 3 to 3 and one half feet between rows. It will take 55 to 60 days to mature.",
+"garden peas": "Plant garden Peas between March 25 and April 10 in the spring. Plant them one and one half to 2 inches deep, 1 inches apart, with 2 and one half feet between rows. It will take 60 to 80 days to mature.",
+"southern peas": "Plant southern Peas between May 1 and 15 in the spring. Plant them one and one half to 2 inches deep, 4 to 6 inches apart, with 3 feet between rows. It will take 60 to 70 days to mature.",
+"pepper": "Plant Pepper between May 15 and 30 in the spring. Plant them one inch deep, one and one half inch to 2 feet apart, with 2 and one half feet between rows. It will take 65 to 80 days to mature.",
+"potatoes": "Plant Potatoes between April 1 and 15 in the spring. Plant them five inches deep, 10 to 14 inches apart, with 2 and one half to 3 feet between rows. It will take 70 to 90 days to mature.",
+"potato": "Plant Potatoes between April 1 and 15 in the spring. Plant them five inches deep, 10 to 14 inches apart, with 2 and one half to 3 feet between rows. It will take 70 to 90 days to mature.",
+"sweet potatoes": "Plant Potato, sweet between May 15 and June 5 in the spring. Plant them five inches deep, 12 inches apart, with 3 and one half feet between rows. It will take 90 to 150 days to mature.",
+"squash": "Plant Squash between May 15 and 30 in the spring. Plant them one and one half to 2 inches deep, 2 feet apart, with 3 to 4 feet between rows. It will take 50 to 55 days to mature.",
+"winter squash": "Plant Winter Squash between May 15 and 30 in the spring. Plant them one and one half to 2 inches deep, 3 feet apart, with 5 feet between rows. It will take 85 to 90 days to mature.",
+"tomato": "Plant Tomato between May 15 and 30 in the spring. Plant them one inch deep, 2 and a half to 3 feet apart, with 3 to 4 feet between rows. It will take 70 to 85 days to mature.",
+"tomatoes": "Plant Tomatoes between May 15 and 30 in the spring. Plant them one inch deep, 2 and a half to 3 feet apart, with 3 to 4 feet between rows. It will take 70 to 85 days to mature.",
+"watermelon": "Plant Watermelon between May 10 and 20 in the spring. Plant them one and one half inches deep, 8 to 10 feet apart, with 10 feet between rows. It will take 80 to 90 days to mature."
+};
