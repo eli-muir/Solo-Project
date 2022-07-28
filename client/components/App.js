2df1f41c322c6526db07a5505185f43c8fddb78e
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-07-28 13:05:12 -0400

    new direction

--- client/components/App.js
+++ client/components/App.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
 
-//create a state object which consists of an empty array called vegetables
 
+
+//create a state object which consists of an empty array called vegetables
 class App extends Component {
-        state = {
-          vegetables: []
+  constructor(){
+    super();
+        this.state = {
+          vegetables: [],
       };
+  }
 
 //Invoked immediately after a component is mounted. Loading data from remote endpoint.
 componentDidMount() {
-  //fetch data at within jsonbin database
-  fetch("https://api.jsonbin.io/v3/b/62e199c1248d43754f07e01f",{
-    method: "GET",
-    headers: {'X-Master-Key':'$2b$10$jXI1kR8OA0QgGAFVlQ.iPO4ZpV5SqGIC78jjp7pogA9.QYpkoN/ye'}
+  //fetch json data
+  fetch("https://data.mongodb-api.com/app/data-xzwtu/endpoint/data/v1",{
+  headers: {
+    'api-key': 'k4fh0lk3n9Z3CZP7EtnZFFFfGSumtTFeCETqmmbRrrmne9G7nRZHmFVIQtumNLLF',
+  }
+
   })
-    .then(response => {
-      console.log(response)
+    .then(function(response) {
       return response.json();
-      }).then(data => {
+      }).then((json) => {
           this.setState({
-            vegetables: data
+            vegetables: json
           })
       })
+      console.log(this.state.vegetables)
 };
 
 render () {
   return <div className='drop-down'>
     <p>Select Your Vegetable to Grow</p>
-      <select>{
-        this.state.vegetables.map((obj) => {
-          return <option> {obj.name} </option>
-        })
-
-      }</select>
   </div>;
 }
 }
 
+// ("https://api.jsonbin.io/v3/b/62e199c1248d43754f07e01f",{
+//     method: "GET",
+//     headers: {'X-Master-Key':'$2b$10$jXI1kR8OA0QgGAFVlQ.iPO4ZpV5SqGIC78jjp7pogA9.QYpkoN/ye'}
+//   })
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
+
 
 
 // statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../dist')));
+// app.use('/build', express.static(path.join(__dirname, '../dist')));
+
+app.post('./veggie', (req,res) => {
+  console.log('vegggiiiesssss')
+})
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join('../index.html'))
 });
+
+
 
 
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
